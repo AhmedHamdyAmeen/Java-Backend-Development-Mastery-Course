b6a1577a4c3dd5760c05fbcfe0b1c57b126474b3
AhmedHamdyAmeen <geoAhmedHamdy1@gmail.com>
2025-11-11 21:12:40 +0200

    PS | Java | HackerRank | Easy | WelcomeToJava

---
+++ src/main/java/PS/hackerrank/WelcomeToJava.java
@@ -0,0 +1,12 @@
+package PS.hackerrank;
+
+/**
+ * URL: https://www.hackerrank.com/challenges/welcome-to-java/problem?isFullScreen=false
+ * Required: copy and past some output lines.
+ */
+public class WelcomeToJava {
+    public static void main(String[] args) {
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
+}
