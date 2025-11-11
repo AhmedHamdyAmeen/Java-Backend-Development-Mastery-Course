e5ccf6bb2a49b6c457c93083e905db18bcb89805
AhmedHamdyAmeen <geoAhmedHamdy1@gmail.com>
2025-11-11 21:51:00 +0200

    PS | Java | HackerRank | Easy | JavaStdinAndStdout

--- src/main/java/PS/hackerrank/PS1WelcomeToJava.java
+++ src/main/java/PS/hackerrank/PS1WelcomeToJava.java
@@ -4,7 +4,7 @@ package PS.hackerrank;
  * URL: https://www.hackerrank.com/challenges/welcome-to-java/problem?isFullScreen=false
  * Required: copy and past some output lines.
  */
-public class WelcomeToJava {
+public class PS1WelcomeToJava {
     public static void main(String[] args) {
         System.out.println("Hello, World.");
         System.out.println("Hello, Java.");
